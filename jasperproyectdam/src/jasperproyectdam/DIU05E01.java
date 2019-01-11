77e8598e00d7240e50882265edcc6954517d58b6
rolandocllbz <34546095+rolandocllbz@users.noreply.github.com>
2019-01-11 20:44:19 +0100

    Add files via upload

---
+++ jasperproyectdam/src/jasperproyectdam/DIU05E01.java
@@ -0,0 +1,20 @@
+package jasperproyectdam;
+
+public class DIU05E01 {
+	//-- Parametros
+	private static String CONF_ACCESS_DB;
+	private static String CONF_ACCESS_PORT;
+	private static String CONF_ACCESS_PATH;
+	private static String desdeFecha;
+	private static String hastaFecha;
+	
+	
+	public static void main(String[] args) {
+		//-- Codigo
+	}
+	private static void leeInit() {}
+	public static void guardarCSV() {}
+	public static void guardarPDF() {}
+	public static void guardarHTML() {}
+	
+}
